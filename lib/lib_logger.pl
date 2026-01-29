d6c2ae7157212f3ad21ef9331d86bcf9b3cedbc5
Nil Geisweiller <ngeiswei@gmail.com>
2026-01-29 09:02:08 +0200

    Add comments to lib_logger.pl

--- lib/lib_logger.pl
+++ lib/lib_logger.pl
@@ -1,5 +1,31 @@
+% Logger for MeTTa based on
+%
+% [log4p](https://www.swi-prolog.org/pack/list?p=log4p)
+%
+% a SWI-Prolog logger.
+%
+% By default it logs a message MESSAGE by appending the following line
+% to the file petta.log in the current directory
+%
+% [DATETIME] [LOGLEVEL] MESSAGE
+%
+% where DATETIME is the current date and time and LOGLEVEL is one the
+% following: TRACE, DEBUG, INFO, WARN, ERROR, FATAL.
+%
+% The logger has a global (across all threads) log level, as well as
+% local (per thread) log levels.  The default log level (global and
+% local) is INFO, meaning that by default all messages logged at the
+% INFO, WARN, ERROR or FATAL level will be logged while the messages
+% logged at the TRACE or DEBUG level will be ignored.
+
 % Import log4p
-use_module(library(log4p)).
+:- use_module(library(log4p)).
+
+% NEXT: support resetting stdout, filepath and timestamp.
+
+% NEXT: test overhead and see if it possible to minimize it.
+
+% NEXT: add lots of comments in lib_logger.metta.
 
 % PeTTa log handler, append timestamped messages to a petta.log file
 % under the current folder
